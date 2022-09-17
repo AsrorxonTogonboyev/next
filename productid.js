b414dd9b9528959cbc7690d0d65a0227fb7e7ea6
AsrorxonTogonboyev <113536772+AsrorxonTogonboyev@users.noreply.github.com>
2022-09-17 15:42:34 +0500

    Add files via upload

---
+++ productid.js
@@ -0,0 +1,207 @@
+export const product =[
+    {
+        id: 1,
+        title:"Patrik",
+        photo: "/produc.webp",
+        catigory:"Dinner"
+        
+    },
+    {
+        id: 2,
+        title:"pitsa",
+        photo: "/produc_1.webp",
+        catigory:"Breakfast"
+        
+    },
+    {
+        id: 3,
+        title:"Patriki",
+        photo: "/produc_2.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 4,
+        title:"Patriki",
+        photo: "/produc_3.webp",
+        catigory:"Breakfast"
+    },
+    {
+        id: 5,
+        title:"Patriki",
+        photo: "/produc_4.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 6,
+        title:"Patriki",
+        photo: "/produc_5.webp",
+        catigory: "Dinner"
+        
+    },   
+    {
+        id: 7,
+        title:"Patrik",
+        photo: "/produc.webp",
+        catigory:"Breakfast"
+        
+    },
+    {
+        id: 8,
+        title:"Patriki",
+        photo: "/produc_1.webp",
+        catigory:"Dinner"
+        
+    },
+    {
+        id: 9,
+        title:"Patriki",
+        photo: "/produc_2.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 10,
+        title:"Patriki",
+        photo: "/produc_3.webp",
+        catigory:"Breakfast"
+    },
+    {
+        id: 11,
+        title:"Patriki",
+        photo: "/produc_4.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 12,
+        title:"Patriki",
+        photo: "/produc_5.webp",
+        catigory: "Dinner"
+        
+    },
+    {
+        id: 13,
+        title:"Patrik",
+        photo: "/produc.webp",
+        catigory:"Breakfast"
+        
+    },
+    {
+        id: 14,
+        title:"Patriki",
+        photo: "/produc_1.webp",
+        catigory:"Dinner"
+        
+    },
+    {
+        id: 15,
+        title:"Patriki",
+        photo: "/produc_2.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 16,
+        title:"Patriki",
+        photo: "/produc_3.webp",
+        catigory:"Breakfast"
+    },
+    {
+        id: 17,
+        title:"Patriki",
+        photo: "/produc_4.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 18,
+        title:"Patriki",
+        photo: "/produc_5.webp",
+        catigory: "Dinner"
+        
+    },
+    {
+        id: 19,
+        title:"Patrik",
+        photo: "/produc.webp",
+        catigory:"Breakfast"
+        
+    },
+    {
+        id: 20,
+        title:"Patriki",
+        photo: "/produc_1.webp",
+        catigory:"Dinner"
+        
+    },
+    {
+        id: 21,
+        title:"Patriki",
+        photo: "/produc_2.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 22,
+        title:"Patriki",
+        photo: "/produc_3.webp",
+        catigory:"Breakfast"
+    },
+    {
+        id: 23,
+        title:"Patriki",
+        photo: "/produc_4.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 24,
+        title:"Patriki",
+        photo: "/produc_5.webp",
+        catigory: "Dinner"
+        
+    },
+    {
+        id: 25,
+        title:"Patrik",
+        photo: "/produc.webp",
+        catigory:"Breakfast"
+        
+    },
+    {
+        id: 26,
+        title:"Patriki",
+        photo: "/produc_1.webp",
+        catigory:"Dinner"
+        
+    },
+    {
+        id: 27,
+        title:"Patriki",
+        photo: "/produc_2.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 28,
+        title:"Patriki",
+        photo: "/produc_3.webp",
+        catigory:"Breakfast"
+    },
+    {
+        id: 29,
+        title:"Patriki",
+        photo: "/produc_4.webp",
+        catigory:"Lunch"
+        
+    },
+    {
+        id: 30,
+        title:"Patriki",
+        photo: "/produc_5.webp",
+        catigory: "Dinner"
+        
+    },
+]
